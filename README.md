b52ac599b451fc2ee7ea2e6e5a7d8f75c3347e6e
Patrick Esser <Patrick.Esser@gmx.net>
2017-05-12 17:21:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,93 @@
+
+## Behavioral Cloning
+
+This project implements behavioral cloning for driving a car in [Udacity's Simulator](https://github.com/udacity/self-driving-car-sim) using [Keras](https://keras.io/). 
+
+### Demo
+![demo](https://giant.gfycat.com/HorribleTintedAmphibian.gif)
+[HQ version](https://gfycat.com/HorribleTintedAmphibian) or see `video.mp4` for two laps of driving.
+
+### Repository
+
+#### Files
+The repository consists of
+
+- `model.py` to prepare data, define, train and save the model.
+- `model.h5` the trained model.
+- `drive.py` used to control the simulator from the trained model. Same as in [original repository](https://github.com/udacity/CarND-Behavioral-Cloning-P3) but speed adjusted to 20.
+- `video.mp4` demo of the trained model driving in the simulator.
+- `README.md` this file which is also my writeup for the project.
+
+#### Useage
+To download the training data, train the model and save the best one (according to the validation loss) run
+
+    python model.py
+
+The model will be saved as `./model.h5`. To test it in the simulator, run
+
+    python drive.py path/to/model.h5
+
+and start [the simulator](https://github.com/udacity/self-driving-car-sim) in autonomous mode. Make sure to remove the generated file `data/data.p` if you want to retrain on modified training data (just put additonal data collected by the simulator into `data/` and `model.py` will automatically incooperate it).
+
+### Model overview
+The beginning of `model.py` is responsible for obtaining the training data (both the provided sample data and an additional dataset). In `prepare_data()`, all the datasets are merged into a convenient data structure containing steering, throttle, brake and speed data together with the corresponding center, left and right images. The data is fed into the network with the help of the generator `FileFlow`, which loads batches of images on the fly, augments them if it is training data and shuffles the dataset after each epoch. Finally, the model is defined in the `Model` class using [Keras](https://keras.io/). It learns a mapping from images to steering commands using the squared error between prediction and ground truth as regression loss.
+
+#### Architecture
+The network first normalizes the image intensities to [-1, 1] and crops them. After that it uses multiple convolutional layers followed by ReLU units. These convolutional layers use an exponentially increasing number of feature maps and a stride of two to keep the computational complexity moderate and to increase the receptive field. Finally, the last convolutional layer produces a single feature map which is then reduced to a single output via global average pooling.
+
+#### Overfitting
+To combat overfitting, data use is maximized by utilizing all three camera images, center, left and right, and flipping the images and steering commands. Moderate dropout is used in all non-output layers. Overfitting is monitored with the help of a validation split and early stopping is implemented by saving the model only if the validation loss improved.
+#### Training
+Training uses the [Adam](https://arxiv.org/abs/1412.6980) optimizer with default parameters to minimize the mean squared error on batches of size 64 for 100 epochs. I consistently find the combination of Adam with ReLU units and [He normalization](https://arxiv.org/abs/1502.01852) to work without problems amongst a large variety of models.
+
+#### Training data
+The [provided sample data](https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip) is used together with [additional data](https://drive.google.com/open?id=0B_2YVqPvaFeTSmtBUDlGcHhTWWc) that I collected. It consists of frames collected mainly in curves while driving two laps in forward direction and one lap in reverse direction to get more variety and counteract a bias towards zero steering command by focusing on curves.
+
+### Design documentation
+I had already gained some experience in behavioral cloning for driving simulators in a previous project (see [presentation](https://drive.google.com/file/d/0B_2YVqPvaFeTQjJ1b2I1Q1NLQXc/view?usp=sharing) and [demo](https://gfycat.com/ResponsibleHeavenlyAiredaleterrier)), where I implemented an end-to-end model using [Caffe](http://caffe.berkeleyvision.org/) for driving in [TORCS](http://torcs.sourceforge.net/). The next sections describe how I arrived at the final implementation.
+
+Get up and running with simple fully connected network using provided sample data. Fix error introduced by the use of different python versions. Move on to convolutional network, motivated by [previous project](https://drive.google.com/file/d/0B_2YVqPvaFeTQjJ1b2I1Q1NLQXc/view?usp=sharing). Not too bad in simulator. First step to both combat overfitting and avoid left/right bias - flip image and corresponding steering angle with probability 0.5. Crop images so network can focus on more relevant parts and without hood in the image we can use all three images. Everything except sharp curve with different texture. Try to improve with dropout then collect more data.
+#### Solution Design
+To get up and running I just used a simple, one layer, fully connected network and the provided sample data to develop a pipeline to go from the data to a model that can be used to control the simulator, without worrying about the model's performance. I used different machines to train the model and test it in the simulator and experienced some problems loading the model on the test machine. I solved it after I noticed that the training machine used Python 3.5 whereas the testing machine ran Python 3.6. Using [conda](https://conda.io/docs/) it was easy to run Python 3.6 also on the training machine, despite the lack of administrator rights, and it fixed the problem.
+With a working pipeline I replaced the network with a convolutional network which already produced reasonable behaviour. To avoid a left bias due to driving the track counterclockwise, I flip the images as well as the corresponding steering angle. Instead of saving this additional data on disk, this is implemented in the generator by flipping each image and steering angle with probability 0.5. To better utilize the dataset I decided to use all three images that are collected in each frame:
+
+![datasamples](http://i.imgur.com/IOcDr2q.png)
+
+First of all I had the network crop the images such that it cannot see the hood anymore because during testing time it will only receive the center image. Since the top part of the image is probably not that useful for predicting steering angles I decided to crop it away as well, that way the network can concentrate on the interesting parts. After cropping the images look like this:
+
+![datasamples cropped](http://i.imgur.com/ZnAuoBa.png)
+
+The provided steering command is with respect to the center image and must be adjusted for the left and right frames. I simply adjusted it by subtracting a constant for the left images and adding it for the right images and this turned out to be a really helpful parameter for training the model because it can be used to adjust how hard the model is steering. Suppose the frame was taken while the car was in the middle of the road with a neutral steering command. If a large correction constant is used, the model will learn to steer hard towards the center, whereas it will think that being off center is just as fine as being centered if the correction constant is zero. Here is how the model behaves with a relatively large correction constant of 1.0:
+
+![oversteering](https://giant.gfycat.com/EquatorialGaseousChinesecrocodilelizard.gif)
+[HQ version](https://gfycat.com/EquatorialGaseousChinesecrocodilelizard)
+
+and here is the same model training with a correction constant of 0.0:
+
+![understeering](https://giant.gfycat.com/BossyGargantuanIndianrockpython.gif)
+[HQ version](https://gfycat.com/BossyGargantuanIndianrockpython)
+
+As can be seen, the parameter can be used to control how smooth the network is driving. After some trial and error I found that a correction constant of 0.12 works as a good middle ground to stay centered without oscillations.
+
+#### Data collection
+At this point the model was already working quite well, except that sometimes it missed the sharp curve shown in the demo at the beginning. Therefore I collected additional data focusing on the curves of the track during two laps going forward and one lap going in the reversed direction. With this additional data, the model managed to stay on track.
+
+#### Final architecture
+
+| Layer       | Type                    |         Details                          |
+|-------------|-------------------------|------------------------------------------|
+| 0           |    Cropping             |   50 pixels from top, 25 from bottom     |
+|             |    Normalization        |   into [-1, 1]                           |
+| i = 1,...,6 |    Convolution          |   kernel size: 3, stride: 2, padded, number of feature maps: 2^i * 32  |
+|             |    Activation           |   ReLU                                   |
+|             |    Dropout              |   rate training: 0.25, rate testing: 0.0 |
+| 7           |    Convolution          |   kernel size: 1, stride: 1, number of feature maps: 1024  |
+|             |    Activation           |   ReLU                                   |
+|             |    Dropout              |   rate training: 0.25, rate testing: 0.0 |
+| 8           |    Convolution          |   kernel size: 1, stride: 1, number of feature maps: 1  |
+|             |    Pooling              |   Global average pooling                 |
+
+
+
+
+> Written with [StackEdit](https://stackedit.io/).
---
+++ model.py
@@ -0,0 +1,330 @@
+import os, urllib.request, sys, math, csv, pickle
+from zipfile import ZipFile
+import numpy as np
+from multiprocessing.pool import ThreadPool
+
+import keras
+import keras.backend as K
+from keras.preprocessing.image import load_img, img_to_array
+
+# path where sample training data will be put
+data_dir = os.path.join(os.getcwd(), "data")
+os.makedirs(data_dir, exist_ok = True)
+
+
+def dl_progress(count, block_size, total_size):
+    """Progress bar used during download."""
+    length = 50
+    current_size = count * block_size
+    done = current_size * length // total_size
+    togo = length - done
+    prog = "[" + done * "=" + togo * "-" + "]"
+    sys.stdout.write(prog)
+    if(current_size < total_size):
+        sys.stdout.write("\r")
+    else:
+        sys.stdout.write("\n")
+    sys.stdout.flush()
+
+
+def download_data():
+    """Download data."""
+    data = {
+            "data.zip": "https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip",
+            "data_curves.zip": "https://drive.google.com/open?id=0B_2YVqPvaFeTSmtBUDlGcHhTWWc"}
+    local_data = {}
+    for fname, url in data.items():
+        path = os.path.join(data_dir, fname)
+        if not os.path.isfile(path):
+            print("Downloading {}".format(fname))
+            urllib.request.urlretrieve(url, path, reporthook = dl_progress)
+        else:
+            print("Found {}. Skipping download.".format(fname))
+        local_data[fname] = path
+    return local_data
+
+
+def extract_data(path):
+    """Extract zip file if not already extracted."""
+    with ZipFile(path) as f:
+        targets = dict((fname, os.path.join(data_dir, fname)) for fname in f.namelist())
+        if not all([os.path.exists(target) for target in list(targets.values())[:10]]): # only check for first 10 for speed
+            print("Extracting {}".format(path))
+            f.extractall(data_dir)
+        else:
+            print("Skipping extraction of {}".format(path))
+    return targets
+
+
+def prepare_data():
+    """Collect data from subdirectories of data dir, merge them, split into
+    training, validation and  testing splits and pickle to disk."""
+    out_path = os.path.join(data_dir, "data.p")
+    if os.path.isfile(out_path):
+        print("Using {}".format(out_path))
+        with open(out_path, "rb") as f:
+            data = pickle.load(f)
+        return data
+
+    csvfname = "driving_log.csv"
+    imgdirname = "IMG"
+    datasets = []
+    for dir_ in os.listdir(data_dir):
+        # paths
+        dir_ = os.path.join(data_dir, dir_)
+        csvpath = os.path.join(dir_, csvfname)
+        imgpath = os.path.join(dir_, imgdirname)
+        if not os.path.isdir(dir_) or not os.path.isfile(csvpath) or not os.path.isdir(imgpath):
+            continue
+
+        # parse csv file into dictionary
+        lines = list()
+        with open(csvpath, "r", newline = "") as f:
+            csvreader = csv.reader(f)
+            for line in csvreader:
+                lines.append(line)
+        if lines[0][0] == "center":
+            keys = lines.pop(0)
+        else:
+            keys = "center,left,right,steering,throttle,brake,speed".split(",")
+        # adapt paths
+        for i, line in enumerate(lines):
+            for col in range(3):
+                fname = os.path.basename(line[col])
+                path = os.path.join(imgpath, fname)
+                lines[i][col] = path
+        data = dict()
+        for i, k in enumerate(keys):
+            data[k] = list()
+            for line in lines:
+                data[k].append(line[i])
+        datasets.append(data)
+    assert(datasets)
+    # merge them
+    all_data = dict((k, list()) for k in keys)
+    for dataset in datasets:
+        for k in keys:
+            all_data[k] += dataset[k]
+    # shuffle
+    n_samples = len(all_data[keys[0]])
+    for v in all_data.values():
+        assert(len(v) == n_samples)
+    shuffled_indices = np.random.permutation(n_samples)
+    for k in all_data.keys():
+        all_data[k] = [all_data[k][i] for i in shuffled_indices]
+    # split
+    n_train = int(0.7 * n_samples)
+    n_valid = int(0.5 * (n_samples - n_train))
+    begins = {
+            "train": 0,
+            "valid": n_train,
+            "test": n_train + n_valid}
+    ends = {
+            "train": n_train,
+            "valid": n_train + n_valid,
+            "test": n_samples}
+    data = dict()
+    for split in ["train", "valid", "test"]:
+        data[split] = dict()
+        for k in keys:
+            data[split][k] = all_data[k][begins[split]:ends[split]]
+    # pickle to disk
+    with open(out_path, "wb") as f:
+        pickle.dump(data, f)
+    print("Wrote {}".format(out_path))
+    return data
+
+
+def table_format(row, header = False, width = 10):
+    """Format row as markdown table."""
+    result = "|" + "|".join(str(entry).center(width) for entry in row) + "|"
+    if header:
+        sep = "".join(map(lambda x: x if x == "|" else "=", result))
+        result = result + "\n" + sep
+    return result
+
+
+def summarize(data):
+    """Summarize size of data for each split."""
+    keys = sorted(list(data["train"].keys()))
+    print("Keys: {}".format(", ".join(keys)))
+    headers = ["Split", "Samples", "Height", "Width", "Channels"]
+    print(table_format(headers, header = True))
+    for split in ["train", "valid", "test"]:
+        d = data[split]
+        n = len(d[keys[0]])
+        sample_image_path = data[split]["center"][0]
+        img = load_img(sample_image_path)
+        x = img_to_array(img)
+        h, w, c = x.shape
+        row = [split, n, h, w, c]
+        print(table_format(row))
+
+
+class FileFlow(object):
+    """Load batches on the fly."""
+
+    def __init__(self, batch_size, data, augment = False):
+        self.data = data
+        self.y = data["steering"]
+        self.batch_size = batch_size
+        self.augment = augment
+        self.n = len(self.y)
+        self.steps_per_epoch = math.ceil(self.n / self.batch_size)
+        # infer shape
+        self.img_shape = img_to_array(load_img(self.data["center"][0])).shape
+        self.shuffle()
+
+
+    def __next__(self):
+        batch_start, batch_end = self.batch_start, self.batch_start + self.batch_size
+        batch_indices = self.indices[batch_start:batch_end]
+        batch_fnames = dict()
+        for pos in ["center", "left", "right"]:
+            batch_fnames[pos] = [self.data[pos][i] for i in batch_indices]
+        batch_y = [float(self.y[i]) for i in batch_indices]
+
+        current_batch_size = len(batch_indices)
+
+        batch = np.zeros((current_batch_size,) + self.img_shape, dtype = K.floatx())
+        for i in range(current_batch_size):
+            if self.augment:
+                # choose one of the camera positions at random
+                pos = np.random.choice(["center", "left", "right"])
+                fname = batch_fnames[pos][i]
+                # and adjust target steering angle
+                if pos == "left":
+                    batch_y[i] += 0.12
+                if pos == "right":
+                    batch_y[i] -= 0.12
+            else:
+                fname = batch_fnames["center"][i]
+            img = load_img(fname)
+            x = img_to_array(img)
+            if self.augment:
+                # with probability 0.5 flip image and steering angle
+                if np.random.randint(2):
+                    x = np.fliplr(x)
+                    batch_y[i] = -batch_y[i]
+            batch[i] = x
+
+        if batch_end > self.n:
+            self.shuffle()
+        else:
+            self.batch_start = batch_end
+
+        return batch, batch_y
+
+
+    def shuffle(self):
+        self.batch_start = 0
+        self.indices = np.random.permutation(self.n)
+
+
+def keras_normalize(x):
+    return x / 127.5 - 1.0
+
+
+def keras_global_average_pooling(x):
+    return K.mean(x, [1,2])
+
+
+class Model(object):
+    """CNN for end-to-end steering angle prediction."""
+
+    def __init__(self, img_shape, n_epochs):
+        self.img_shape = img_shape
+        self.learning_rate = 1e-3
+        self.decay = self.learning_rate / n_epochs
+        self.dr_rate = 0.25
+        self.n_epochs = n_epochs
+        self.define_model()
+        self.init_model()
+
+
+    def define_model(self):
+        n_features = 32
+        kernel_size = 3
+        activation = "relu"
+        kernel_initializer = "he_normal"
+
+        x = keras.layers.Input(shape = self.img_shape)
+        features = keras.layers.Cropping2D(cropping = ((50,25), (0,0)))(x)
+        features = keras.layers.Lambda(keras_normalize)(features)
+
+        for i in range(6):
+            features = keras.layers.Convolution2D(
+                    filters = 2**i * n_features,
+                    kernel_size = kernel_size,
+                    strides = 2,
+                    padding = "SAME",
+                    activation = activation,
+                    kernel_initializer = kernel_initializer)(features)
+            features = keras.layers.Dropout(self.dr_rate)(features)
+
+        features = keras.layers.Convolution2D(
+                filters = 1024,
+                kernel_size = 1,
+                strides = 1,
+                padding = "SAME",
+                activation = activation,
+                kernel_initializer = kernel_initializer)(features)
+        features = keras.layers.Dropout(self.dr_rate)(features)
+        features = keras.layers.Convolution2D(
+                filters = 1,
+                kernel_size = 1,
+                strides = 1,
+                padding = "SAME",
+                activation = None,
+                kernel_initializer = kernel_initializer)(features)
+
+        output = keras.layers.Lambda(keras_global_average_pooling)(features)
+        self.model = keras.models.Model(x, output)
+
+
+    def init_model(self):
+        optimizer = keras.optimizers.Adam(lr = self.learning_rate, decay = self.decay)
+        self.model.compile(loss = "mse", optimizer = optimizer)
+        ckpt_path = os.path.join(data_dir, "model.h5")
+        checkpoint = keras.callbacks.ModelCheckpoint(
+                ckpt_path,
+                monitor = "val_loss",
+                verbose = 1,
+                save_best_only = True)
+        self.callbacks = [checkpoint]
+
+
+    def fit(self, batches, valid_batches = None):
+        self.history = self.model.fit_generator(
+                generator = batches,
+                steps_per_epoch = batches.steps_per_epoch,
+                epochs = self.n_epochs,
+                validation_data = valid_batches,
+                validation_steps = valid_batches.steps_per_epoch,
+                callbacks = self.callbacks)
+
+
+    def test(self, batches):
+        return self.model.evaluate_generator(
+                generator = batches,
+                steps = batches.steps_per_epoch)
+
+
+if __name__ == "__main__":
+    files = download_data()
+    extract_data(files["data.zip"])
+    data = prepare_data()
+    summarize(data)
+
+    batch_size = 64
+    train_batches = FileFlow(batch_size, data["train"], augment = True)
+    valid_batches = FileFlow(batch_size, data["valid"])
+    test_batches = FileFlow(batch_size, data["test"])
+
+    img_shape = train_batches.img_shape
+    n_epochs = 100
+
+    model = Model(img_shape, n_epochs)
+    model.fit(train_batches, valid_batches)
+    test_loss = model.test(test_batches)
+    print("Testing loss: {:.4e}".format(test_loss))
